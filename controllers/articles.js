59d67840e09862d70a1b2e6aadca18d6775a437d
Csepregi <csepregigabor02@gmail.com>
2020-02-27 22:56:11 +0100

    scrape linked pages

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,33 +1,45 @@
 const articlesRouter = require('express').Router()
 const cheerio = require('cheerio')
-const axios = require('axios')
+// const axios = require('axios')
+const request = require('request-promise')
 
-articlesRouter.get('/', async (request, response) => {
-
-	axios.get('https://blog.risingstack.com/').then((res) => {
-		const articles = [];
-		const $ = cheerio.load(res.data);
-		$('.post-header').each((index, element) => {
-			const title = $(element)
-				.children()
-				.first()
-				.text()
-			const link = $(element)
-				.children()
-				.children()
-				.attr('href')
-			articles[index] = { title, link }
-			const data = await axios.get(`https://blog.risingstack.com${link}`)
-			console.log(data)
-		})
-
-
-		console.log(articles)
-		response.json(articles)
+articlesRouter.get('/', async (req, res) => {
+	const baseUrl = 'https://blog.risingstack.com'
+	const mainhtml = await request(baseUrl)
+	const articles = [];
+	const $ = cheerio.load(mainhtml);
+	$('.post-header').each((index, element) => {
+		const title = $(element)
+			.children()
+			.first()
+			.text()
+		const link = baseUrl + $(element)
+			.children()
+			.children()
+			.attr('href')
+		articles[index] = { title, link }
 	})
 
+	const data = await Promise.all(articles.map(async (article) => {
+		try {
+			const experthtml = await request(article.link);
+			const $ = cheerio.load(experthtml)
 
+			const titles = $('h2')
+			const smallTitles = titles.map((i, title) => {
+				return $(title).text()
+			}).get()
+			return {
+				title: article.link,
+				smallTitles
+			}
+		} catch (error) {
 
+		}
+	}))
+
+	console.log(data)
+	res.json(data)
 });
 
 
